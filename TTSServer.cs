5d08e5883b584d094e7f96d881a193ceae33da2f
zhangtao <zhangtao.1994@bytedance.com>
2024-06-12 19:46:32 +0800

    rename files

--- TTSServer.cs
+++ TTSServer.cs
@@ -10,17 +10,10 @@ namespace DRGSoundPad
         public static async Task DownloadFile(string text, string filePath)
         {
             using var client = new HttpClient();
-            // 发送 GET 请求并获取响应消息
             var response = await client.GetAsync(baseUrl + text);
-
-            // 确认响应成功
             response.EnsureSuccessStatusCode();
-
-            // 从响应消息中读取文件内容并写入到文件
             await using var fileStream = new FileStream(filePath, FileMode.Create);
             await response.Content.CopyToAsync(fileStream);
         }
-
-
     }
 }
